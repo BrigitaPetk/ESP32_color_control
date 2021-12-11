8d77b4f811daf9aa1675b5e79b648087adf22300
Brigita <brigita.kalendraite@gmail.com>
2021-12-11 16:15:28 +0200

    Basic wifi connection

--- board/main.py
+++ board/main.py
@@ -1,6 +1,27 @@
 import time
 from machine import Pin
 from neopixel import NeoPixel
+import json
+import time
+import network
+
+
+# Prieeiga prie ID ir slaptazodziu:
+with open('secret.json') as f:
+    config = json.load(f)
+
+# Prisijungimas prie wifi
+wifi = network.WLAN(network.STA_IF)
+wifi.active(True)
+print(wifi.scan())                             # Nuskanuojamos galimo interneto prieeigos
+wifi.connect(config["ssid"], config["ssid_password"])       # Prisijungimas
+
+while not wifi.isconnected():
+    time.sleep(1)
+
+print("Prisijungta sekmingai")
+
+
 
 pin = Pin(27, Pin.OUT)  # set GPIO0 to output to drive NeoPixels
 np = NeoPixel(pin, 2)  # create NeoPixel driver on GPIO0 for 8 pixels
