8b4cafd5ce6b6b5094843aaf9d844f7388a08534
Brigita <brigita.kalendraite@gmail.com>
2021-12-18 17:37:00 +0200

    pixel number, wifi and diagram update

--- board/main.py
+++ board/main.py
@@ -14,37 +14,42 @@ with open('secret.json') as f:
 
 # Prisijungimas prie wifi
 wifi = network.WLAN(network.STA_IF)
-wifi.active(True)
-print(wifi.scan())                             # Nuskanuojamos galimo interneto prieeigos
-wifi.connect(config["ssid"], config["ssid_password"])       # Prisijungimas
+    
+def prisijungimas():
+    wifi.active(True)
+    print(wifi.scan())                             # Nuskanuojamos galimo interneto prieeigos
+    wifi.connect(config["ssid"], config["ssid_password"])       # Prisijungimas
 
-while not wifi.isconnected():
-    time.sleep(1)
+    while not wifi.isconnected():
+        time.sleep(1)
 
-print("Prisijungta sekmingai")
-print(wifi.ifconfig())
+    return "Prisijungta sekmingai"
+    # print(wifi.ifconfig())
 
 
 # Nurodymai neopixeliui:
-pin = Pin(27, Pin.OUT)      # 27 jungtis(GPIO0) atitinka T7 jungti ant mikroschemos
-np = NeoPixel(pin, 1)        # nurodoma, kuri jungtis ir kiek Neopixeliu prijungta
+pin = Pin(14, Pin.OUT)      # 27 jungtis(GPIO0) atitinka T7 jungti ant mikroschemos
+np = NeoPixel(pin, 3)        # nurodoma, kuri jungtis ir kiek Neopixeliu prijungta
 
 
 while True:
-    # RGB spalvos gavimas is serverio:
-    uzklausos_atsakymas = urequests.get("http://192.168.1.124:5000/spalva")
-    print(f"Užklausos statusas: {uzklausos_atsakymas.status_code}")
-    r_g_b = uzklausos_atsakymas.json()
-    print(r_g_b)
-    r = r_g_b["r"]
-    g = r_g_b["g"]
-    b = r_g_b["b"]
+    if wifi.isconnected():
+        # RGB spalvos gavimas is serverio:
+        uzklausos_atsakymas = urequests.get("http://192.168.8.117:5000/spalva")
+        print(f"Užklausos statusas: {uzklausos_atsakymas.status_code}")
+        r_g_b = uzklausos_atsakymas.json()
+        print(r_g_b)
+        r = r_g_b["r"]
+        g = r_g_b["g"]
+        b = r_g_b["b"]
 
-    np[0] = (r, g, b) 
-    np.write()    
+        for index, _  in enumerate(np):
+            np[index] = (r, g, b)
+            np.write()    
 
-    time.sleep(5)        
+        time.sleep(5)        
     
-
+    else:
+        prisijungimas()
 
 
