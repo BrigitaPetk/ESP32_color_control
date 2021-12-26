4f3cf0353d72bcef32982c5e0228313953d95574
Brigita <brigita.kalendraite@gmail.com>
2021-12-26 21:11:04 +0200

    url and wifi changes

--- board/main.py
+++ board/main.py
@@ -9,7 +9,7 @@ import urequests
 
 # Prieeiga prie ID ir slaptazodziu:
 with open('secret.json') as f:
-    config = json.load(f)
+    kodai = json.load(f)
 
 
 # Prisijungimas prie wifi
@@ -17,35 +17,34 @@ wifi = network.WLAN(network.STA_IF)
     
 def prisijungimas():
     wifi.active(True)
-    print(wifi.scan())                             # Nuskanuojamos galimo interneto prieeigos
-    wifi.connect(config["ssid"], config["ssid_password"])       # Prisijungimas
+    wifi.connect(kodai["ssid"], kodai["ssid_password"]) 
 
     while not wifi.isconnected():
         time.sleep(1)
 
-    return "Prisijungta sekmingai"
+    print("Prisijungta sekmingai")
     # print(wifi.ifconfig())
 
 
 # Nurodymai neopixeliui:
-pin = Pin(14, Pin.OUT)      # 27 jungtis(GPIO0) atitinka T7 jungti ant mikroschemos
+pin = Pin(14, Pin.OUT)      # 14 jungtis(GPIO0) atitinka T6 jungti ant mikroschemos
 np = NeoPixel(pin, 3)        # nurodoma, kuri jungtis ir kiek Neopixeliu prijungta
 
 
 while True:
     if wifi.isconnected():
         # RGB spalvos gavimas is serverio:
-        uzklausos_atsakymas = urequests.get("http://192.168.8.117:5000/spalva")
+        uzklausos_atsakymas = urequests.get("http://192.168.8.109:5000/spalva")
         print(f"Užklausos statusas: {uzklausos_atsakymas.status_code}")
         r_g_b = uzklausos_atsakymas.json()
         print(r_g_b)
         r = r_g_b["r"]
         g = r_g_b["g"]
         b = r_g_b["b"]
-
+        # Pixeliu uzpildymas spalva:
         for index, _  in enumerate(np):
             np[index] = (r, g, b)
-            np.write()    
+            np.write()  
 
         time.sleep(5)        
     
